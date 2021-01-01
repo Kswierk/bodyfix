8c6e65a242043b462d5e92f10d52c914fa77b02a
Kacper Świerk <kacper.swierk@gmail.com>
2021-01-01 15:06:06 +0100

    removed unnecessary files and added navbar folder structure

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import Button from '../Button';
+import Dropdown from './Dropdown';
+
+import styled from 'styled-components';
+
+const Navbar = () => {
+  return <div></div>;
+};
+
+export default Navbar;
